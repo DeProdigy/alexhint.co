18bf1ace957d60a3417b747a7c5e97cc40148717
DeProdigy <alex.hint.ah@gmail.com>
2013-12-08 22:58:37 -0500

    new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AlexhintCo::Application.config.secret_token = '27799fc8d53ed677e18ce5c303ab8e77c5bb85ba61f4793e220c33581c16003892168516a768de3427414e4fa73ed797e5d3fbcabf537c6ea28d361f12d48e5c'
