9b80604c9cb3e6b2bf64d9699c06a35a98f947b5
DeProdigy <alex.hint.ah@gmail.com>
2013-12-10 21:16:55 -0500

    added descriptions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,20 @@
 module ApplicationHelper
   def github_link(link)
-    link_to image_tag('github.png', class: 'github'), 'https://github.com/DeProdigy' + link, class: 'link', target: 'blank'
+    link_to image_tag('github.png', class: 'icon'), 'https://github.com/DeProdigy' + link, class: 'link', target: 'blank'
+  end
+
+  def twitter_link
+    link_to image_tag('twitter.png', class: 'icon'), 'https://twitter.com/hintology', class: 'link', target: 'blank'
+  end
+
+  def mail_link
+    # mail_to "me@domain.com"
+    mail_to 'alex.hint.ah@gmail.com', image_tag('mail.png', class: 'icon')
   end
 
   def project_link(image, link)
     link_to image_tag(image, class: 'greyscale'), link, target: 'blank'
   end
+
+
 end
