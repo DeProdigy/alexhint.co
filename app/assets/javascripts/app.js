f105b8b3887a296dcc66e4173691964622145b10
DeProdigy <alex.hint.ah@gmail.com>
2014-01-13 14:03:50 -0500

    change one scroll menu color

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -10,7 +10,6 @@ $(document).ready(function($) {
      loop: false, // You can have the page loop back to the top/bottom when the user navigates at up/down on the first/last page.
      responsiveFallback: false // You can fallback to normal page scroll by defining the width of the browser in which you want the responsive fallback to be triggered. For example, set this to 600 and whenever the browser's width is less than 600, the fallback will kick in.
   });
-
     eventListeners();
 });
 
