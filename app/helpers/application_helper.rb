e5b56ad411fbe4dea1784f4b255755704b782360
DeProdigy <alex.hint.ah@gmail.com>
2014-01-13 04:44:57 -0500

    added svg icons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,22 @@
 module ApplicationHelper
   def github_link(link)
-    link_to image_tag('github.png', class: 'icon'), 'https://github.com/DeProdigy' + link, class: 'link', target: 'blank'
+    link_to image_tag('github.svg', class: 'icon'), 'https://github.com/DeProdigy' + link, class: 'link', target: 'blank'
   end
 
   def twitter_link
-    link_to image_tag('twitter.png', class: 'icon'), 'https://twitter.com/hintology', class: 'link', target: 'blank'
+    link_to image_tag('twitter.svg', class: 'icon'), 'https://twitter.com/hintology', class: 'link', target: 'blank'
   end
 
   def mail_link
-    mail_to 'alex.hint.ah@gmail.com', image_tag('mail.png', class: 'icon')
+    mail_to 'alex.hint.ah@gmail.com', image_tag('mail.svg', class: 'icon')
+  end
+
+  def instagram_link
+    link_to image_tag('instagram.svg', class: 'icon'), 'http://instagram.com/hintography', class: 'link', target: 'blank'
+  end
+
+  def linkedin_link
+    link_to image_tag('linkedin.svg', class: 'icon'), 'http://linkedin.com/in/alexhint/', class: 'link', target: 'blank'
   end
 
   # def project_link(image, link)
