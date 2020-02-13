a70e3f126971b308e6df8d0287a23bf163e26218
DeProdigy <alex.hint.ah@gmail.com>
2020-02-13 02:01:00 -0500

    Update resume link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,7 +17,7 @@ module ApplicationHelper
   end
 
   def resume_link
-    link_to image_tag('briefcase.png', class: 'animated fadeInUp icon'), 'https://drive.google.com/file/d/0B87BKNuErC3qOEJkSnQzVFNKT28/view?usp=sharing', class: 'link', target: 'blank'
+    link_to image_tag('briefcase.png', class: 'animated fadeInUp icon'), 'https://drive.google.com/file/d/1_91uqe1N-ZMOv_I0ShuIZxULW0bYfPrC/view', class: 'link', target: 'blank'
   end
 
   def linkedin_link
